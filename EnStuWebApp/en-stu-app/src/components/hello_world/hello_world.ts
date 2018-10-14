2075d90385eda1885a1a8374cab29d04c1dd0093
Phong Do <phongdo@innoria.com>
2018-10-14 17:35:55 +0700

    add vuetify

--- EnStuWebApp/en-stu-app/src/components/hello_world/hello_world.ts
+++ EnStuWebApp/en-stu-app/src/components/hello_world/hello_world.ts
@@ -2,6 +2,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 import template from './hello_world.vue';
 
 @Component({
+    name: 'HelloWorld',
     mixins: [template]
 })
 export default class HelloWorld extends Vue {
